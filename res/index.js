cb78968321add5ab5ba45fbf5a3b048d615e958c
root <brian_chuk@yahoo.com>
2015-08-17 17:30:32 -0400

    feat(icons): added key words with regex for weather icons :sunny:

--- res/index.js
+++ res/index.js
@@ -1,33 +1,51 @@
-function average(a, b) {
-	return (a + b)/2;
-}
+var BASE_URL = "https://api.syncano.io/v1/instances/weathered-river-7002/webhooks/p/e3f72fc463d20d5dd1a173efc6e8a74ae44a977e/";
 
 function grabDayData(data, index) {
 	return data[index][Object.keys(data[index])]
 }
 
-$( '.exit' ).click(function() {
-	$(this).parent().parent().remove();
-});
+function setWeatherIcon(element, description) {
+	var keyWords = [
+		{keyWord:'mist', wi: 'wi wi-day-fog'},
+		{keyWord:'fog', wi: 'wi wi-day-fog'},
+		{keyWord:'clear', wi: 'wi wi-day-sunny'},
+		{keyWord:'drizzle', wi: 'wi wi-day-sprinkle'},
+		{keyWord:'rain', wi: 'wi wi-day-rain'},
+		{keyWord:'thunderstorm', wi: 'wi wi-day-thunderstorm'},
+		{keyWord:'snow', wi: 'wi wi-day-snow'}
+	];
+	for (var i = 0; i < keyWords.length; i++) {
+		var regex = new RegExp(keyWords[i].keyWord, 'i');
+		if (description.match(regex)) {
+			$(element).removeClass().addClass(keyWords[i].wi);
+			break;
+		} else {
+			$(element).removeClass().addClass(keyWords[2].wi);
+		}
+	}
+}
 
 $( ".waves-effect.waves-teal.btn-flat" ).click(function() {
-	city = 'city=' + $( '#city' ).val().split(' ').join('%20');
+	city = '?city=' + $( '#city' ).val().split(' ').join('%20');
 	state = '&state=' + $( '#state' ).val();
-
 
 	var fetchData = new XMLHttpRequest();
 	// var url = "https://api.syncano.io/v1/instances/weathered-river-7002/webhooks/p/e3f72fc463d20d5dd1a173efc6e8a74ae44a977e/?city=New%20york&state=ny";
-	var url = "https://api.syncano.io/v1/instances/weathered-river-7002/webhooks/p/e3f72fc463d20d5dd1a173efc6e8a74ae44a977e/?" + city + state;
+	var url = BASE_URL + city + state;
+	
+	// Setup GET Request to fetch weather data
 	fetchData.open("GET", url, true);
 	fetchData.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	fetchData.onreadystatechange = function() {//Call a function when the state changes.
+	fetchData.onreadystatechange = function() {
 		if(fetchData.readyState == 4 && fetchData.status == 200) {
 		    var data = JSON.parse(JSON.parse(fetchData.responseText).result.stdout);
 		    $neWbox = $( "#original" ).clone().appendTo( ".weatherWrapper" );
 			$neWbox.removeAttr('id');
 			
 			$( '.exit' ).click(function() {
-				$(this).parent().parent().remove();
+				$(this).parent().parent().fadeOut(function() {
+					$(this).parent().parent().remove();
+				}.bind());
 			});
 
 			var today = grabDayData(data, 0);
@@ -35,9 +53,10 @@ $( ".waves-effect.waves-teal.btn-flat" ).click(function() {
 			var dayThree = grabDayData(data, 2);
 			var dayFour = grabDayData(data, 3);
 
-			console.log(tomorrow);
+			console.log(data);
 
 			// $neWbox.find('h2').text("New York");
+			// Set weather box data
 			$neWbox.find('h2').text($( '#city' ).val());
 			$neWbox.find('.wTemperature').text(today.current_temp_fahrenheit);			
 			$neWbox.find('.wDay').text(Object.keys(data[0])[0]);
@@ -57,6 +76,11 @@ $( ".waves-effect.waves-teal.btn-flat" ).click(function() {
 			$('<sup>°<span>F</span></sup>').appendTo($neWbox.find('.wiMin'));
 			$('<sup>°<span>F</span></sup>').appendTo($neWbox.find('.wiMax'));
 			$('<sup>°<span>F</span></sup>').appendTo($neWbox.find('.wTemperature'));
+
+			setWeatherIcon($neWbox.find('.wi.wi-alien')[0], today.more_descriptive_description);
+			setWeatherIcon($neWbox.find('.wi.wi-alien')[0], tomorrow.more_descriptive_description);
+			setWeatherIcon($neWbox.find('.wi.wi-alien')[0], dayThree.more_descriptive_description);
+			setWeatherIcon($neWbox.find('.wi.wi-alien')[0], dayFour.more_descriptive_description);
 
 			$neWbox.fadeIn();
 		}
